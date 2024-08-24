800256e50dc801470d0e4d70eb1b436af8f18df7
PrajwalMReddy <prajwalmsreddy@gmail.com>
2024-08-23 22:49:19 -0400

    Add A Rudimentary Kannada Language Translation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+let languageIndexes = {
+    "ಕನ್ನಡ": "ಮನೆ.html",
+    "English": "index.html"
+};
+
 function languageChoiceChange() {
     let languageChoice = document.getElementById("nav-language-choice");
     let language = languageChoice.options[languageChoice.selectedIndex].text;
+
     localStorage.setItem("language", language);
+    window.location.href = languageIndexes[language];
 }
