5148946b74415cc5777a45ec586816a1cbfae47c
pmr93 <pmr93@cornell.edu>
2024-10-15 17:12:22 -0400

    Modify The Language Changer JS Code To Be More Robust

--- src/index.js
+++ src/index.js
@@ -1,32 +1,17 @@
 let languageCodes = {
-    "kn": "ಕನ್ನಡ",
-    "en": "English",
+    "kn": "ಕನ್ನಡ", "en": "English",
 };
 
-let languageIndexes = {
-    "ಕನ್ನಡ": {
-        "index": "/kn/mane.html",
-        "projects": "/kn/yojanegalu.html",
-        "blog": "/kn/mimbaraha.html",
-        "contact": "/kn/samparka.html",
-    },
-    "English": {
-        "mane": "/en/index.html",
-        "yojanegalu": "/en/projects.html",
-        "mimbaraha": "/en/blog.html",
-        "samparka": "/en/contact.html",
-    },
-};
-
+// Changing the language of a page on load if preferred language is different from loaded page's language
 window.addEventListener("load", (event) => {
     let fullURL = window.location.href;
     let currentPage = fullURL.substring(fullURL.lastIndexOf('/') + 1, fullURL.lastIndexOf('.'));
 
     let preferredLanguage = localStorage.getItem("language");
-    let currentLanguage = fullURL.substring(fullURL.lastIndexOf('/', fullURL.lastIndexOf('/') - 1) + 1, fullURL.lastIndexOf('/'));
+    let currentLanguage = languageCodes[fullURL.substring(fullURL.lastIndexOf('/', fullURL.lastIndexOf('/') - 1) + 1, fullURL.lastIndexOf('/'))];
 
-    if ((preferredLanguage !== languageCodes[currentLanguage])) {
-        window.location.href = fullURL.substring(0, fullURL.lastIndexOf('/', fullURL.lastIndexOf('/') - 1)) + languageIndexes[preferredLanguage][currentPage];
+    if ((preferredLanguage !== currentLanguage)) {
+        loadPreferredLanguagePage(fullURL, preferredLanguage, currentPage);
     }
 });
 
@@ -39,5 +24,36 @@ function languageChoiceChange() {
     let currentPage = fullURL.substring(fullURL.lastIndexOf('/') + 1, fullURL.lastIndexOf('.'));
 
     localStorage.setItem("language", language);
-    window.location.href = fullURL.substring(0, fullURL.lastIndexOf('/', fullURL.lastIndexOf('/') - 1)) + languageIndexes[language][currentPage];
+    loadPreferredLanguagePage(fullURL, language, currentPage);
+}
+
+let languageIndexes = {
+    "ಕನ್ನಡ": {
+        "index": "/kn/mane",
+        "projects": "/kn/yojanegalu",
+        "blog": "/kn/mimbaraha",
+        "contact": "/kn/samparka",
+    }, "English": {
+        "mane": "/en/index",
+        "yojanegalu": "/en/projects",
+        "mimbaraha": "/en/blog",
+        "samparka": "/en/contact",
+    },
+};
+
+function loadPreferredLanguagePage(url, newLanguage, currentPage) {
+    let preferredLanguagePage = languageIndexes[newLanguage][currentPage];
+
+    if (preferredLanguagePage === undefined) {
+        return; // To be decided how to handle this case
+    }
+
+    let urlStem = "";
+    if (preferredLanguagePage.includes("blog") || preferredLanguagePage.includes("mimbaraha")) {
+        urlStem = url.substring(0, url.lastIndexOf('/', (url.lastIndexOf('/', url.lastIndexOf('/') - 1) - 1)));
+    } else {
+        urlStem = url.substring(0, url.lastIndexOf('/', url.lastIndexOf('/') - 1));
+    }
+
+    window.location.href = urlStem + preferredLanguagePage + ".html";
 }
