107383ce220ca9787d04602d4a7d24b8e8194e32
pmr93 <pmr93@cornell.edu>
2025-06-16 13:52:00 +0530

    Make The Website More Responsive

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
 import {useLanguage} from '../utils/LanguageContext';
 import LanguageSwitcher from './LanguageSwitcher';
 
 const SideNav = () => {
     const {t, language} = useLanguage();
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <nav id="nav-div">
-            <ul id="nav-list">
-                <li id="nav-main"><Link to="/" className="nav-link">{t('navName')}</Link></li>
-                <li className="nav-element"><Link to="/projects" className="nav-link">{t('project')}</Link></li>
-                <li className="nav-element"><Link to="/blog" className="nav-link">{t('blog')}</Link></li>
-                <li className="nav-element"><Link to="/contact" className="nav-link">{t('contact')}</Link></li>
-            </ul>
-            <LanguageSwitcher/>
-        </nav>
+        <>
+            <button 
+                className="hamburger-menu" 
+                onClick={toggleMenu}
+                aria-label="Toggle menu"
+            >
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+            <nav id="nav-div" className={isOpen ? 'open' : ''}>
+                <ul id="nav-list">
+                    <li id="nav-main"><Link to="/" className="nav-link" onClick={() => setIsOpen(false)}>{t('navName')}</Link></li>
+                    <li className="nav-element"><Link to="/projects" className="nav-link" onClick={() => setIsOpen(false)}>{t('project')}</Link></li>
+                    <li className="nav-element"><Link to="/blog" className="nav-link" onClick={() => setIsOpen(false)}>{t('blog')}</Link></li>
+                    <li className="nav-element"><Link to="/contact" className="nav-link" onClick={() => setIsOpen(false)}>{t('contact')}</Link></li>
+                </ul>
+                <LanguageSwitcher/>
+            </nav>
+            {isOpen && <div className="overlay" onClick={toggleMenu}></div>}
+        </>
     );
 };
 
