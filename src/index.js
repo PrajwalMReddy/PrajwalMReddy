4b2ebd55a803035af0cd4e368fde63b898d86055
pmr93 <pmr93@cornell.edu>
2025-06-16 11:57:35 +0530

    Try A Few Changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import './assets/favicon/hurricane.ico'; // Import the favicon
 
 const container = document.getElementById('root');
 const root = createRoot(container);
